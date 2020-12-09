abdd95faa8199fb10ceb6cff30bc5ec88cdcd260
Bohdan00735 <bodya00735@gmail.com>
2020-12-09 22:01:40 +0200

    post+search

--- app/src/main/java/com/example/ranrideroutes/MainActivity.kt
+++ app/src/main/java/com/example/ranrideroutes/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        actionBar?.hide();
+        supportActionBar?.hide()
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
